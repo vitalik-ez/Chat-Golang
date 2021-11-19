6c51baff772c84fe05e6bf91a2f85dfe19f94538
vitalik-ez <vetalyeshor@gmail.com>
2021-11-19 02:27:18 +0200

    Add test

--- pkg/handler/chat_test.go
+++ pkg/handler/chat_test.go
@@ -1,11 +1,12 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/gorilla/websocket"
 	"github.com/vitalik-ez/Chat-Golang/pkg/service"
 	mock_service "github.com/vitalik-ez/Chat-Golang/pkg/service/mocks"
@@ -16,12 +17,13 @@ func httpToWs(t *testing.T, url string) string {
 	return "ws" + url[len("http"):]
 }
 
-func WSServer(t *testing.T, h http.Handler) (*httptest.Server, *websocket.Conn) {
+func WSServer(t *testing.T, hb *hub, h *Handler) (*httptest.Server, *websocket.Conn) {
 	t.Helper()
 
-	s := httptest.NewServer(h)
+	s := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		h.chatRoomWS(hb, rw, r)
+	}))
 	wsURL := httpToWs(t, s.URL)
-	fmt.Println(wsURL)
 	ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
 	if err != nil {
 		t.Fatal(err)
@@ -30,45 +32,58 @@ func WSServer(t *testing.T, h http.Handler) (*httptest.Server, *websocket.Conn) 
 	return s, ws
 }
 
-
 func sendMessage(t *testing.T, ws *websocket.Conn, command HubCommand) {
 	t.Helper()
-
 	if err := ws.WriteJSON(command); err != nil {
 		t.Fatalf("%v", err)
 	}
 }
 
-
 func TestHandler_chatRoomWS(t *testing.T) {
-	type mockBehavior func(s *mock_service)
-	hub := NewHub()
-	h = Handler{services: &service.Service{}}
-	s, ws := WSServer(t, h.chatRoomWS(hub))
-	defer s.Close()
-	defer ws.Close()
+	type mockBehavior func(s *mock_service.MockRoom, room string)
 
-	testTable := []struct{
-		name string
-		command HubCommand
+	testTable := []struct {
+		name         string
+		command      HubCommand
 		mockBehavior mockBehavior
-	} {
+	}{
 		{
 			name: "join to the room",
-			command: &HubCommand{
+			command: HubCommand{
 				UserName: "Vitaliy",
-				Command: "join",
-				Room: "kpi",
+				Command:  "join",
+				Room:     "kpi",
 			},
-			mockBehavior: func(s *mock_service.)
+			mockBehavior: func(s *mock_service.MockRoom, room string) {
+				s.EXPECT().Create(room).Return(nil)
+			},
 		},
 	}
 
 	for _, testCase := range testTable {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(testCase.name, func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
 
+			room := mock_service.NewMockRoom(c)
+			testCase.mockBehavior(room, testCase.command.Room)
+
+			services := &service.Service{Room: room}
+			h := NewHandler(services)
+
+			hub := NewHub()
+			s, ws := WSServer(t, hub, h)
+			defer s.Close()
+			defer ws.Close()
+			go hub.Run()
+
+			sendMessage(t, ws, testCase.command)
+			time.Sleep(time.Second)
+
+			if _, ok := hub.Rooms[testCase.command.Room]; !ok {
+				t.Fatalf("Expected '%+v' in Room map", testCase.command.Room)
+			}
 		})
 	}
 
-	
 }
